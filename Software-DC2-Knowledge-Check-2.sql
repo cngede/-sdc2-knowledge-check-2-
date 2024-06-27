9ed0919d91bac084f0c5829fdd0e24a80f7f1f9a
Cajetan Ngede <134719269+cngede@users.noreply.github.com>
2024-06-27 18:19:53 -0400

    Update Software-DC2-Knowledge-Check-2.sql

--- Software-DC2-Knowledge-Check-2.sql
+++ Software-DC2-Knowledge-Check-2.sql
@@ -4,7 +4,7 @@ From Orders
 Inner join Customers on Orders.CustomerID=Customers.CustomerID
 where OrderID=10310
 
-
+ 
 Select Products.ProductID, Products.SupplierID, Suppliers.Address
 From Products
 Inner join Suppliers on Products.SupplierID= Suppliers.SupplierID
